18c2b60d3e9f3470af4614e39ac3bece6e3ce2a9
danymcboockair <dsmmgc@gmail.com>
2024-10-18 14:06:38 +0100

    ejercicio 03

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -6,20 +6,71 @@ https://www.youtube.com/live/euVbF1eatEY?si=nHS3QznAlA_C39NK&t=808
 
 // 1. Escribe un comentario en una línea
 
+// Esto es un comentario.
+
 // 2. Escribe un comentario en varias líneas
+
+/*Esto es un comentario 
+de varias lineas*/ 
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+var myVar = "Var ya no se usa"
+
+let myNumber = 10  
+let myText = "Hola JavaScript"
+let myBoolean = true
+const pi = 3.14  //no mutable
+
 // 4. Imprime por consola el valor de todas las variables
+
+console.log(myNumber);
+console.log(myText);
+console.log(myBoolean);
+console.log(pi);
 
 // 5. Imprime por consola el tipo de todas las variables
 
+console.log(typeof myVar);
+console.log(typeof myNumber);
+console.log(typeof myText);
+console.log(typeof myBoolean);
+console.log(typeof pi);
+
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
-// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+myVar = "Ahora ya se usa menos"
+myText = "Hey"
+myBoolean = false
+myNumber = 5
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo®
+
+myVar = 3
+myText = 10
+myBoolean = "hola"
+myNumber = "Mi número"
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
+const myConstNumber = 3.14
+const myConstText = "id"
+const myConstBoolean = true
+console.log(myConstNumber);
+console.log(myConstText);
+console.log(myConstBoolean);
+
 // 9. A continuación, modifica los valores de las constantes
 
+// dan error, no se pueden modificar
+
+// myConstBoolean = false
+// myConstNumber = "se cambia?"
+// myConstText = 19
+
+
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+console.log(myConstBoolean);
+console.log(myConstText);
+console.log(myConstNumber);
